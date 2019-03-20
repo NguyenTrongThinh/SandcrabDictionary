3a878a21c0467f516ab4665bd0fdeafc9c2f3d96
ThinhNguyen <nguyen.trong.thinh@styl.solutions>
2019-03-20 17:17:42 +0700

    Redesign Searchbar
    Signed-off-by: ThinhNguyen <nguyen.trong.thinh@styl.solutions>

--- app/src/main/java/com/solo/sandcrabdictionary/activities/MainActivity.java
+++ app/src/main/java/com/solo/sandcrabdictionary/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package com.solo.sandcrabdictionary.activities;
 
 import android.app.ActivityOptions;
-import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
@@ -11,16 +10,13 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageView;
 
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.solo.sandcrabdictionary.R;
 import com.solo.sandcrabdictionary.databinding.ActivityMainBinding;
 import com.solo.sandcrabdictionary.fragments.RandomWordsFragment;
@@ -42,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
     private RandomWordsFragment randomWordsFragment;
     private WordDetailsFragment wordDetailsFragment;
     private FragmentManager fragmentManager;
-    private SearchView searchView;
-    private EditText searchViewEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,10 +49,67 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initActionBar() {
-        setSupportActionBar(binding.activityMainToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_blue_light_24dp);
-        binding.activityMainToolbar.setTitle(getResources().getString(R.string.app_name));
+        binding.activityMainsearchBar.setHint(getResources().getString(R.string.text_demo_word));
+        binding.activityMainsearchBar.setSpeechMode(true);
+        binding.activityMainsearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                if (enabled) {
+                    if (wordDetailsFragment.getVisibility() == View.GONE) {
+                        recentWordsFragment.setVisibility(View.VISIBLE);
+                        randomWordsFragment.setVisibility(View.GONE);
+                    } else {
+                        wordDetailsFragment.setVisibility(View.GONE);
+                        recentWordsFragment.setVisibility(View.VISIBLE);
+                        randomWordsFragment.setVisibility(View.GONE);
+                    }
+                } else {
+                    if (wordDetailsFragment.getVisibility() == View.GONE) {
+                        recentWordsFragment.setVisibility(View.VISIBLE);
+                        randomWordsFragment.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                binding.activityMainLoading.show();
+                lookUpWord(text.toString());
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+                switch (buttonCode){
+                    case MaterialSearchBar.BUTTON_NAVIGATION:
+                        binding.activityMainDrawerLayout.openDrawer(GravityCompat.START);
+                        break;
+                    case MaterialSearchBar.BUTTON_SPEECH:
+                        break;
+                    case MaterialSearchBar.BUTTON_BACK:
+                        binding.activityMainsearchBar.disableSearch();
+                        break;
+                }
+            }
+        });
+
+        binding.activityMainsearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                recentWordsFragment.filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private void initFragments() {
@@ -99,21 +150,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-        if (intent != null && Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-
-            searchView.setQuery(query, true);
-        }
-    }
-
     private void lookUpWord(String word) {
         final OxfordDictionaryInterface oxfordDictionaryInterface = OxfordDictionaryClient.getService();
         final Call<OxfordWord> callback = oxfordDictionaryInterface.lookUpWord(word);
@@ -126,8 +162,7 @@ public class MainActivity extends AppCompatActivity {
                 randomWordsFragment.setVisibility(View.GONE);
                 wordDetailsFragment.setPageContent(oxfordWord);
                 wordDetailsFragment.setVisibility(View.VISIBLE);
-                searchViewEditText.setText("");
-                searchView.clearFocus();
+                binding.activityMainsearchBar.disableSearch();
             }
 
             @Override
@@ -137,85 +172,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search_view, menu);
-        MenuItem menuItem = menu.findItem(R.id.mnuSearch);
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) menuItem.getActionView();
-        ImageView searchViewIcon = searchView.findViewById(android.support.v7.appcompat.R.id.search_mag_icon);
-        searchViewEditText = searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
-        searchViewEditText.setSelectAllOnFocus(true);
-        ViewGroup linearLayoutSearchView =(ViewGroup) searchViewIcon.getParent();
-        linearLayoutSearchView.removeView(searchViewIcon);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false);
-
-        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    if (wordDetailsFragment.getVisibility() == View.GONE) {
-                        recentWordsFragment.setVisibility(View.VISIBLE);
-                        randomWordsFragment.setVisibility(View.GONE);
-                    }
-                }
-                else {
-                    if (wordDetailsFragment.getVisibility() == View.GONE) {
-                        recentWordsFragment.setVisibility(View.VISIBLE);
-                        randomWordsFragment.setVisibility(View.VISIBLE);
-                    }
-                }
-            }
-        });
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-                binding.activityMainLoading.show();
-                lookUpWord(searchViewEditText.getText().toString());
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                recentWordsFragment.filter(newText);
-                return false;
-            }
-        });
-        handleIntent(getIntent());
-        return true;
-    }
 
     @Override
     public void onBackPressed() {
-        if (binding.activityMainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+        if (binding.activityMainsearchBar.isSearchEnabled()) {
+            binding.activityMainsearchBar.disableSearch();
+        } else if (binding.activityMainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             binding.activityMainDrawerLayout.closeDrawers();
         } else if (wordDetailsFragment.getVisibility() == View.VISIBLE) {
-            searchViewEditText.setText("");
             wordDetailsFragment.setVisibility(View.GONE);
             recentWordsFragment.setVisibility(View.VISIBLE);
             randomWordsFragment.setVisibility(View.VISIBLE);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                try {
-                    binding.activityMainDrawerLayout.openDrawer(GravityCompat.START);
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
